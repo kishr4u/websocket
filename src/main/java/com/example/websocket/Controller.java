71b0f56b2cded1c2512e0e36173c05d34901ec48
Kishore Jagannath <kishore.jagannath@microfocus.com>
2022-01-02 10:17:24 +0530

    files

---
+++ src/main/java/com/example/websocket/Controller.java
@@ -0,0 +1,18 @@
+package com.example.websocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    // Handles messages from /app/chat. (Note the Spring adds the /app prefix for us).
+    @MessageMapping("/chat")
+    // Sends the return value of this method to /topic/messages
+    @SendTo("/topic/messages")
+    public Message getMessages(Message dto){
+
+        return dto;
+
+    }
+}
